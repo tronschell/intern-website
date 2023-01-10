4cdf1a6ee443389f03a90b0f34e98ca48d4c2e27
Tron Schell <zoniixyt@gmail.com>
2023-01-09 21:44:48 -0500

    added more placeholder items to the hero page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,28 +20,38 @@
   import "carbon-components-svelte/css/g90.css";
   
   let isSideNavOpen = false;
+  const imgUrl = new URL('/Users/tronschell/Documents/JS/intern-website/static/business-dashboard-example.png', import.meta.url).href
 
 </script>
 
 <style>
-  .hero-background{
+
+  .dashboard-image{
+    object-fit: contain;
+    max-width: 50vw;
+    border-color: aliceblue;
+    border-width: 2px;
+    border-style: solid;
+  }
+
+  body{
     opacity: 0.8;
-  background-image:  radial-gradient(#638fb4 0.6000000000000001px, transparent 0.6000000000000001px), radial-gradient(#638fb4 0.6000000000000001px, transparent 0.6000000000000001px);
+    background-image:  radial-gradient(#638fb4 0.6000000000000001px, transparent 0.6000000000000001px), radial-gradient(#638fb4 0.6000000000000001px, transparent 0.6000000000000001px);
   background-size: 24px 24px;
   background-position: 0 0,12px 12px;
-  z-index:9;
-  max-width: 100%;
-  max-width: 100%;
+    
   }
 
   .hero-section{
-    margin-top:10rem;
+    padding-top: 10rem;
+    
     display:flex;
     justify-content:center;
     align-items: center;
     flex-direction: column;
     gap:3rem;
-
+    
+    max-width: 100vw;
   }
 
   .hero-text{
@@ -70,24 +80,57 @@
     align-items: center;
     gap:1rem;
   }
-</style>
-<Header platformName="test" bind:isSideNavOpen>
 
-  <SideNav>
-    <SideNavItems>
-      <SideNavLink>Test</SideNavLink>
-    </SideNavItems>
-  </SideNav>
-</Header>
-<div class="hero-background">
-  <div class="hero-section">
-    <h1 class="hero-text">Streamline the Internship / Co-op job searching experience.</h1>
-    <h4 class="hero-description">Delete those endless Chrome tabs by compiling all aspects of the job search experience into one platform.</h4>
+  .info-section-one{
+    background-color: black;
+    gap:1rem; 
+    width:50vw;
+  }
+
+  .info-section-one-textblock{
+    display: flex;
+    flex-direction: row;
     
-    <div class="hero-button-section">
-      <Button tooltipAlignment="center">Sign Up</Button>
-      <Button kind="tertiary">Learn More</Button>
+  }
+
+  .info-section-one-text{
+    text-align: center;
+    display:flex;
+    width:45rem;
+    height:auto;
+    font-size: 16px;
+    color:gainsboro;
+    transition: ease-in 0.7s;
+  }
+</style>
+
+  <Header platformName="test" bind:isSideNavOpen>
+
+    <SideNav>
+      <SideNavItems>
+        <SideNavLink>Test</SideNavLink>
+      </SideNavItems>
+    </SideNav>
+  </Header>
+<body>
+  <div class="hero-background">
+    <div class="hero-section">
+      <h1 class="hero-text">Optimize the Internship / Co-op job search experience.</h1>
+      <h4 class="hero-description">Delete those endless Chrome tabs by compiling all aspects of the job search experience into one unified platform.</h4>
+      
+      <div class="hero-button-section">
+        <Button tooltipAlignment="center">Sign Up</Button>
+        <Button kind="tertiary">Learn More</Button>
+      </div>
+
+      <img class="dashboard-image" src={imgUrl} alt="placeholder-image">
     </div>
   </div>
-</div>
 
+  <div class="info-section-one">
+    <div class="info-section-one-textblock">
+      <h4 class="info-section-one-text">Find jobs from multiple different sources including company websites, Indeed.com, Glassdoor.com, and more.</h4>
+    </div>
+    
+  </div>
+</body>
